5abe7f271135b55dfe8352a8b6446d019a86c03d
Gabriel Pereira <gabrielpereiram10@gmail.com>
2022-07-31 12:44:24 -0300

    refactor: verificar se email é válido

--- user-service/src/main/java/com/gabriel/microservices/userservice/domain/valueObjects/Email.java
+++ user-service/src/main/java/com/gabriel/microservices/userservice/domain/valueObjects/Email.java
@@ -6,7 +6,11 @@ public class Email {
 
     private final String value;
     public Email(String email) {
-        throw new InvalidFieldException("email", "Email inválido.");
+        if (!email.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+            throw new InvalidFieldException("email", "Email inválido.");
+        }
+
+        this.value = email;
     }
 
     public String getValue() {
