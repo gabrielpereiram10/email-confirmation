b707b62fd3fca1aae1dbf971fabd1685de1fa79d
Gabriel Pereira <gabrielpereiram10@gmail.com>
2022-07-31 13:43:07 -0300

    feat: verificar se senha contém letra maiúscula e minúscula

--- user-service/src/main/java/com/gabriel/microservices/userservice/domain/valueObjects/Password.java
+++ user-service/src/main/java/com/gabriel/microservices/userservice/domain/valueObjects/Password.java
@@ -6,8 +6,18 @@ public class Password {
 
     private final String value;
     public Password(String password) {
+        String field = "senha";
+        String baseMessage = "Senha deve conter pelo menos";
         if (password.length() < 6) {
-            throw new InvalidFieldException("senha", "Senha deve conter pelo menos 6 caracteres.");
+            throw new InvalidFieldException(field, baseMessage + " 6 caracteres.");
+        }
+
+        if (!password.matches("^.*[A-Z]+.*$")) {
+            throw new InvalidFieldException(field, baseMessage + " uma letra maiúscula.");
+        }
+
+        if (!password.matches("^.*[a-z]+.*$")) {
+            throw new InvalidFieldException(field, baseMessage + " uma letra minúscula.");
         }
 
         this.value = password;
--- user-service/src/test/java/com/gabriel/microservices/userservice/domain/PasswordTest.java
+++ user-service/src/test/java/com/gabriel/microservices/userservice/domain/PasswordTest.java
@@ -16,4 +16,22 @@ public class PasswordTest {
                 () -> assertEquals("Senha deve conter pelo menos 6 caracteres.", exception.getMessage())
         );
     }
+
+    @Test
+    void senhaDeverConterPeloMenosUmaLetraMaiuscula() {
+        InvalidFieldException exception = assertThrows(InvalidFieldException.class, () -> new Password("aaaaaa"));
+        assertAll(
+                () -> assertEquals("senha", exception.getFiled()),
+                () -> assertEquals("Senha deve conter pelo menos uma letra maiúscula.", exception.getMessage())
+        );
+    }
+
+    @Test
+    void senhaDeverConterPeloMenosUmaLetraMinuscula() {
+        InvalidFieldException exception = assertThrows(InvalidFieldException.class, () -> new Password("AAAAAA"));
+        assertAll(
+                () -> assertEquals("senha", exception.getFiled()),
+                () -> assertEquals("Senha deve conter pelo menos uma letra minúscula.", exception.getMessage())
+        );
+    }
 }
